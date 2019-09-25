564c5f59b4582a1c5fa0797c5f2529c833a0e935
Julian M. Rice <rice.m.julian@gmail.com>
2019-09-24 17:52:47 -0700

    Updates

---
+++ precache-manifest.73488cc58ab9fa71110994b0e9c12843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178caa724e2344af9ffa7955241811dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bb1ff176cf2a97dde0c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6555e129104ec7efd011",
+    "url": "/static/js/2.4e8da748.chunk.js"
+  },
+  {
+    "revision": "0bb1ff176cf2a97dde0c",
+    "url": "/static/js/main.250bd136.chunk.js"
+  },
+  {
+    "revision": "2f18bc0cdb0a32f018d3",
+    "url": "/static/js/runtime-main.97c9a2e9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
